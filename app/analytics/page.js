d307333c39e2677a71cfb36e2f31608c86e6e6e0
dark-shadowblade <150048154+dark-shadowblade@users.noreply.github.com>
2025-09-20 02:54:27 +0530

    Update page.js

--- app/analytics/page.js
+++ app/analytics/page.js
@@ -1,8 +1,10 @@
+import TrendAnalysis from "./TrendAnalysis";
+
 export default function Analytics() {
   return (
     <div style={{ padding: '2rem', paddingBottom: '5rem' }}>
       <h1>Analytics Page</h1>
-      <p>Data analytics and charts will be displayed here.</p>
+      <TrendAnalysis />
     </div>
   );
 }
